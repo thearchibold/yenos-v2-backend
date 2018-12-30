e57957a36f40d7e44e51a4f3c4d3c55e76080ea2
theArchibold <e.archibold@webforgegh.com>
2018-12-30 14:36:33 +0000

    finally working

--- app.py
+++ app.py
@@ -18,7 +18,8 @@ def hello_world():
 @app.route('/api_v1/gender/get_gender', methods=['GET'])
 def getGender():
     name_data = request.args.get("name")
-
+    if len(name_data) <= 0:
+        return "Name cannot be empty"
     if special_match(str(name_data)):
         return "Invalid Parameter"
 
