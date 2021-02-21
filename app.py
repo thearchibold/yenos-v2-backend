9e724251e91d47793f34a042c258f882f7217a65
Archibold Bernard <bernard.baidoo@rancard.com>
2021-02-21 05:29:44 +0000

    cors setup

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ import re
 from flask import Flask, request, render_template
 from GenderDetector import detectGender
 from flask import jsonify
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)
 
 
 def special_match(strg, search=re.compile(r"[^a-zA-Z\'\"]").search):
@@ -17,6 +19,7 @@ def hello_world():
     return render_template('landing.html')
 
 @app.route('/api_v1/gender/get_gender', methods=['GET'])
+@cross_origin()
 def getGender():
     name_data = request.args.get("name")
     if len(name_data) <= 0:
